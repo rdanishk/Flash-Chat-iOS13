cfa7dc085cc57e5e7eba316ca6d7bcffa1d4c75c
danish khalid <jhelumgg@yahoo.com>
2020-07-15 11:50:28 -0400

    finished up

--- Flash Chat iOS13/SceneDelegate.swift
+++ Flash Chat iOS13/SceneDelegate.swift
@@ -2,8 +2,8 @@
 //  SceneDelegate.swift
 //  Flash Chat iOS13
 //
-//  Created by Angela Yu on 21/10/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
+//  Created by Danish Khalid on 21/10/2019.
+//  Copyright © 2019 Danish Khalid. All rights reserved.
 //
 
 import UIKit
